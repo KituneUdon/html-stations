d4f9614ad1ec2d35c46054013ff40530e24952ec
KituneUdon <kituneudon97@gmail.com>
2021-06-08 17:47:26 +0900

    RailwayのJS12を実施

--- src/scripts/station12-common.js
+++ src/scripts/station12-common.js
@@ -1,3 +1,8 @@
+// localfileのimportはcorsで動かないので直接ここに書いた
+function getData() {
+  return "Station12をクリア!";
+}
+
 var result = getData();
 var elem = document.getElementById("result");
 elem.innerHTML = result;
