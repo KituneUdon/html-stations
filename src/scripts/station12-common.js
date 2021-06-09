f0a8b865949c1ce62e58e417e85c977c59b9c873
KituneUdon <kituneudon97@gmail.com>
2021-06-09 09:35:35 +0900

    RailwayのJS12の解法が求められている方法と違う方法だったため修正

--- src/scripts/station12-common.js
+++ src/scripts/station12-common.js
@@ -1,8 +1,3 @@
-// localfileのimportはcorsで動かないので直接ここに書いた
-function getData() {
-  return "Station12をクリア!";
-}
-
 var result = getData();
 var elem = document.getElementById("result");
 elem.innerHTML = result;
